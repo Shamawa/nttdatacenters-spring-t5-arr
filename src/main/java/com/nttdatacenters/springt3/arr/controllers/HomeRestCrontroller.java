a4d88ec0683f93506ea8583c46c90e25c2855d98
Alexandra Rodriguez <arodriguez@example.com>
2022-12-22 17:56:04 +0100

    Taller 5 Spring
    Cambios en clase HomeRestCrontroller

--- src/main/java/com/nttdatacenters/springt3/arr/controllers/HomeRestCrontroller.java
+++ src/main/java/com/nttdatacenters/springt3/arr/controllers/HomeRestCrontroller.java
@@ -1,20 +1,21 @@
 package com.nttdatacenters.springt3.arr.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.nttdatacenters.springt3.arr.persistence.Cliente;
 import com.nttdatacenters.springt3.arr.persistence.ClienteRepositoryI;
 
-@Controller
+@RestController
 @RequestMapping(path = "/cliente")
-public class ClienteController {
+public class HomeRestCrontroller {
+	
 	@Autowired
 	private ClienteRepositoryI clienteRepository;
 
